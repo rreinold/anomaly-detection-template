a7bcaa911c8941fc60689325a0637db069ba8b5c
Robert Reinold <rreinold@gmail.com>
2018-05-01 16:18:57 -0400

    Asset created by rreinold

---
+++ code/libraries/AnomalyDetectionConstants/AnomalyDetectionConstants.js
@@ -0,0 +1,24 @@
+// Constants that pertain to Anomaly Detection Template
+
+// see https://github.com/rreinold/twilio-sms-library for Twilio Setup Instructions
+// example
+// user: "BC218b72987d86855a5adb921370115a20"
+// pass: "4579ac6ba4fae7b452c03c64aeae40e7"
+// source: "(+1 512-713-2783)"
+
+const TWILIO_CONFIG = {
+    USER: "", 
+    PASS:"",
+    SOURCE_NUMBER:"",
+    TEMPLATE:""
+}
+
+// see https://github.com/rreinold/clearblade-sendgrid-email-integration for SendGrid Setup Instructions
+// example
+// AUTH_TOKEN:"SG.asldksjdgkljsdbgkdjfbgRMIo7_nsqd8clLfHGQPPDZologFWY73i4"
+// ORIGIN_EMAIL:"example@example.com"
+const SENDGRID_CONFIG = {
+    AUTH_TOKEN:"",
+    ORIGIN_EMAIL:"",
+    TEMPLATE:""
+}
